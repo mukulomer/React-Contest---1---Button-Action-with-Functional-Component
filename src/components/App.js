c2e0458dac06b5412c369ef6dc91346c0b438f41
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-10-21 20:55:47 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,22 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [classname, setClassname] = React.useState("para");
-
+  const [para, setpara] = React.useState(<p></p>);
   const handelClick = () => {
-    let classN = "enable";
-    setClassname(classN);
+    const newPara = (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+    setpara(newPara);
   };
   return (
     <div id="main">
       <button id="click" onClick={handelClick}>
         Click
       </button>
-      <p className={classname} id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
+      {para}
     </div>
   );
 }
