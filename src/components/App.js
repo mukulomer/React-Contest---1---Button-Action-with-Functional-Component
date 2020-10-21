6c6ccc7925f56f28f9f6cdffbac1df1335de258d
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-10-21 20:37:46 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [classname, setClassname] = React.useState("para");
+
+  const handelClick = () => {
+    let classN = "enable";
+    setClassname(classN);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handelClick}>
+        Click
+      </button>
+      <p className={classname} id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
     </div>
   );
 }
-
 
 export default App;
